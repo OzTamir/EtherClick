17c499561224a3e5b49c77e412e3a3583d3e627a
Oz Tamir <oz@blockaid.co>
2022-11-10 20:13:02 +0200

    Added icon and py2app

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ CHROME_PATH = 'open -a /Applications/Google\ Chrome.app %s'
 class EtherClick(rumps.App):
     @rumps.clicked("Etherscan")
     def prefs(self, _):
+        address = xerox.paste()
+        if not address.startswith('0x'):
+            rumps.notification(title='EtherClick', subtitle='Clipboard does not contain a valid ethereum address', message='')
+            return
         url = 'https://etherscan.io/address/' + xerox.paste()
         webbrowser.get(CHROME_PATH).open_new_tab(url)
 
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+APP = ['main.py']
+DATA_FILES = []
+OPTIONS = {
+    'argv_emulation': True,
+    'iconfile': 'ether.icns',
+    'plist': {
+        'CFBundleShortVersionString': '0.2.0',
+        'LSUIElement': True,
+    },
+    'packages': ['rumps', 'xerox'],
+}
+setup(
+    app=APP,
+    name='EtherClick',
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'], install_requires=['rumps', 'xerox']
+)
